1a511aef5b2871877b3dc67fc9129481e7c0cfc3
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-02-07 21:41:04 +0530

    created GET and POST request endpoints

--- main.py
+++ main.py
@@ -11,3 +11,25 @@ async def root():
     Root method: init
     """
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    """
+    Method to return specific item_id data
+    """
+    return {"item_id": item_id}
+
+@app.post("/items/{item_id}")
+async def post_item(item_id: int):
+    """
+    Method to post items
+    """
+    todos.append(item_id)
+    return {"message": f"item: {item_id} added to list"}
+
+@app.post("/items/")    
+async def get_item():
+    """
+    Method to post items
+    """
+    return {"items": todos}
